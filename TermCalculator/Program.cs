80686638c59f9f2621edb464f6ece97b0dbfc51e
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2023-10-13 17:49:46 +0200

    test sin function

--- TermCalculator/Program.cs
+++ TermCalculator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using TermCalculator;
 
-Expression e = Parser.ParseExpression("sin(pi/2)");
+Expression e = Parser.ParseExpression("sin(1.575)");
 e.EvaluateExpression().DisplayExpression("Final Content");
 //e.DisplayExpression("Parsed expression");
 Console.WriteLine(Math.Sin(Math.PI/2));
